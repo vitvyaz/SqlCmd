c490e90b7aa61f3a75847c6003dd6f6b64a5bb70
Vitalii Viazovoi <vitvyaz@gmail.com>
2016-08-24 22:33:57 +0300

    Made new branch JSP. Add web.xml and made class MainServlet

---
+++ src/main/java/ua/com/juja/vitvyaz/sqlcmd/controller/web/MainServlet.java
@@ -0,0 +1,10 @@
+package ua.com.juja.vitvyaz.sqlcmd.controller.web;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Виталий on 24.08.2016.
+ */
+public class MainServlet extends HttpServlet {
+
+}
